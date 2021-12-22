f63d700da53ae2057e064e4aa382d4fabfe8dea1
Adam Angell <Adam.Angell@networkingb2a.com>
2021-12-22 06:38:59 +0000

    Add notes about SDLC and removed old documents

--- Cisco DevNet Associate exam/Sprints/Sprint 1/palindrome.py
+++
@@ -1,6 +0,0 @@
-user_input = input("Please enter a word to find out if its a palindrome or not: ")
-reverse_input = user_input[::-1]
-if user_input ==  reverse_input:
-    print ("this is a palindrome")
-else:
-    print("This is not palindrome")
